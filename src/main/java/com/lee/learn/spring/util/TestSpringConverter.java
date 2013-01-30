7b2adc18ca87e0452064cfbebd1c79e08495badc
madwenoma <madwenoma@gmail.com>
2013-01-30 18:46:49 +0800

    update 2013-01-30

---
+++ src/main/java/com/lee/learn/spring/util/TestSpringConverter.java
@@ -0,0 +1,40 @@
+package com.lee.learn.spring.util;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.core.convert.support.ConversionServiceFactory;
+import org.springframework.core.convert.support.GenericConversionService;
+
+/**
+ * 本类用于测试spring的类型转化功能
+ * @author user
+ *
+ */
+public class TestSpringConverter {
+	private	GenericConversionService service;
+	
+	@Before
+	public void setUp() {
+		service = ConversionServiceFactory.createDefaultConversionService();
+	}
+	
+	
+	@Test
+	public void demoStringToBoolean() {
+		Assert.assertTrue(service.canConvert(String.class, Boolean.class));
+		String boolStr = "true";
+		Assert.assertEquals(true, service.convert(boolStr, Boolean.class));
+		boolStr = "off";
+		Assert.assertEquals(false, service.convert(boolStr, Boolean.class));
+	}
+	
+	@Test
+	public void demoStringToInteger() {
+		Assert.assertTrue(service.canConvert(String.class, Integer.class));
+		String intStr = "100ss";
+		System.out.println(service.convert(intStr, Integer.class));
+	}
+	
+	
+}
--- src/main/java/com/lee/learn/spring/util/TestSpringUtils.java
+++ src/main/java/com/lee/learn/spring/util/TestSpringUtils.java
@@ -1,6 +1,7 @@
 package com.lee.learn.spring.util;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import junit.framework.Assert;
 
@@ -37,6 +38,10 @@ class Animal{
 	public void methodOne() {
 		System.out.println("method one invoked");
 	}
+	public int methodTwo(String words) {
+		System.out.println("method two invoked," + words);
+		return 100;
+	}
 }
 
 public class TestSpringUtils {
@@ -44,16 +49,63 @@ public class TestSpringUtils {
 	@Test
 	public void demoReflectUtils() {
 		Animal dog = new Animal(100, "dog");
-		Assert.assertEquals(100, getFieldValue(dog, "weight"));
+		Assert.assertEquals(100,   getFieldValue(dog, "weight"));
 		Assert.assertEquals("dog", getFieldValue(dog, "name"));
+		setFieldValue(dog, "name", "dogOne");
+		Assert.assertEquals("dogOne", getFieldValue(dog, "name"));
+		invokeMethod(dog, "methodOne");
+		Assert.assertEquals(100, invokeMethod(dog, "methodTwo", "haha"));
 	}
 	
+	/**
+	 * 
+	 * @param obj
+	 * @param fieldName
+	 * @return
+	 */
 	public Object getFieldValue(Object obj, String fieldName) {
 		Field field = ReflectionUtils.findField(obj.getClass(), fieldName);
 		ReflectionUtils.makeAccessible(field);
 		return ReflectionUtils.getField(field, obj);
 	};
 	
+	/**
+	 * 
+	 * @param target
+	 * @param fieldName
+	 * @param value
+	 */
+	public void setFieldValue(Object target, String fieldName, Object value) {
+		Field field = ReflectionUtils.findField(target.getClass(), fieldName);
+		ReflectionUtils.makeAccessible(field);
+		ReflectionUtils.setField(field, target, value);
+	}
 	
+	/**
+	 * 
+	 * @param target
+	 * @param methodName
+	 * @param args
+	 * @return
+	 */
+	public Object invokeMethod(Object target, String methodName, Object... args){
+		Method method = null;
+		if (args == null || args.length == 0) {
+			method = ReflectionUtils.findMethod(target.getClass(), methodName);
+			ReflectionUtils.makeAccessible(method);
+			return ReflectionUtils.invokeMethod(method, target);
+		} else {
+			Class<?>[] classes = new Class[args.length];
+			for (int i = 0; i < classes.length; i++) {
+				classes[i] = args[i].getClass();
+			}
+			method = ReflectionUtils.findMethod(target.getClass(), methodName, classes);
+			ReflectionUtils.makeAccessible(method);
+			return ReflectionUtils.invokeMethod(method, target, args);
+		}
+	}
 	
+	@Test
+	public void demoStringUtils() {
+	}
 }
