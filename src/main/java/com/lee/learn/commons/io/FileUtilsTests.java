f07a3f8f2007645c75ad58134c3c603b47e6f689
madwenoma <madwenoma@gmail.com>
2013-02-01 18:20:00 +0800

    add io test files

---
+++ src/main/java/com/lee/learn/commons/io/FileUtilsTests.java
@@ -0,0 +1,9 @@
+package com.lee.learn.commons.io;
+
+public class FileUtilsTests {
+	public static void main(String[] args) {
+		
+	}
+	
+	
+}
---
+++ src/main/java/com/lee/learn/commons/io/IOUtilsTests.java
@@ -0,0 +1,8 @@
+package com.lee.learn.commons.io;
+
+
+public class IOUtilsTests {
+	public static void main(String[] args) {
+		//IOUtils io = null;
+	}
+}
